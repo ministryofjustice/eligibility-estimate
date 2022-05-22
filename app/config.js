a5f3163a2e5c83f24c6d586780d28dd8836ef1ca
lelpalfrey <leslie.palfrey@digital.justice.gov.uk>
2022-05-22 15:25:23 +0100

    additions to some of the question pages

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Check a clients eligibility for legal aid',
+  serviceName: 'Check eligibility for legal aid',
 
   // Default port that prototype runs on
   port: '3000',
