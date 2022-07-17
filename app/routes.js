61bf9fbcfae9bd817787bb5e4637700917b47efc
lelpalfrey <leslie.palfrey@digital.justice.gov.uk>
2022-07-17 22:08:44 +0100

    Additions to key pages
    jump links added to check answers and question pages.
    
    Vehicle pages logic added to routes.js

--- app/routes.js
+++ app/routes.js
@@ -36,57 +36,106 @@ router.post('/ownsHome', function (req, res) {
 
   })
 
-  // Run this code when a form is submitted to 'ownsHomePartner'
-  router.post('/ownsHomePartner', function (req, res) {
+// Run this code when a form is submitted to 'ownsHomePartner'
+router.post('/ownsHomePartner', function (req, res) {
 
-    // Make a variable and give it the value from 'homeowner'
-    var ownsHomePartner = req.session.data['partnerhomeowner']
+  // Make a variable and give it the value from 'homeowner'
+  var ownsHomePartner = req.session.data['partnerhomeowner']
 
-    // Check whether the variable matches a condition
-    if (ownsHomePartner == "No"){
-      // Skip property q's and go to vehicle q's
-      res.redirect('/partner-vehicle-1')
-    } else {
-      // Send user to property q's to answer all financial q's
-      res.redirect('/partner-property-entry')
-    }
+  // Check whether the variable matches a condition
+  if (ownsHomePartner == "No"){
+    // Skip property q's and go to vehicle q's
+    res.redirect('/partner-vehicle-1')
+  } else {
+    // Send user to property q's to answer all financial q's
+    res.redirect('/partner-property-entry')
+  }
 
-    })
+  })
 
-  // Run this code when a form is submitted to 'ownsVehicle'
-  router.post('/ownsVehicle', function (req, res) {
+// Run this code when a form is submitted to 'ownsVehicle'
+router.post('/ownsVehicle', function (req, res) {
 
-    // Make a variable and give it the value from 'client-vehicle-own'
-    var ownsVehicle = req.session.data['client-vehicle-own']
+  // Make a variable and give it the value from 'client-vehicle-own'
+  var ownsVehicle = req.session.data['client-vehicle-own']
 
-    // Check whether the variable matches a condition
-    if (ownsVehicle == "No"){
-      // Skip vehicle q's and go to assets q's
-      res.redirect('/assets')
-    } else {
-      // Send user to vehicle q's to answer all financial q's
-      res.redirect('/client-vehicle-2')
-    }
+  // Check whether the variable matches a condition
+  if (ownsVehicle == "No"){
+    // Skip vehicle q's and go to assets q's
+    res.redirect('/assets')
+  } else {
+    // Send user to vehicle q's to answer all financial q's
+    res.redirect('/client-vehicle-2')
+  }
 
-    })
+  })
 
-    // Run this code when a form is submitted to 'ownsVehiclePartner'
-    router.post('/ownsVehiclePartner', function (req, res) {
+// Run this code when a form is submitted to 'ownsVehiclePartner'
+router.post('/ownsVehiclePartner', function (req, res) {
 
-      // Make a variable and give it the value from 'partner-vehicle-own'
-      var ownsVehiclePartner = req.session.data['partner-vehicle-own']
+  // Make a variable and give it the value from 'partner-vehicle-own'
+  var ownsVehiclePartner = req.session.data['partner-vehicle-own']
 
-      // Check whether the variable matches a condition
-      if (ownsVehiclePartner == "No"){
-        // Skip vehicle q's and go to assets q's
-        res.redirect('/assets-partner')
-      } else {
-        // Send user to vehicle q's to answer all financial q's
-        res.redirect('/partner-vehicle-2')
-      }
+  // Check whether the variable matches a condition
+  if (ownsVehiclePartner == "No"){
+    // Skip vehicle q's and go to assets q's
+    res.redirect('/assets-partner')
+  } else {
+    // Send user to vehicle q's to answer all financial q's
+    res.redirect('/partner-vehicle-2')
+  }
 
-      })
+  })
 
+// Run this code when a form is submitted to 'regularUseVehicle'
+router.post('/regularUseVehicle', function (req, res) {
+
+  // Make a variable and give it the value from 'client-vehicle-use'
+  var regularUseVehicle = req.session.data['client-vehicle-use']
+
+  // Check whether the variable matches a condition
+  if (regularUseVehicle == "No"){
+    // Skip vehicle q's and go to assets q's
+    res.redirect('/assets')
+  } else {
+    // Send user to next vehicle q to answer all financial q's
+    res.redirect('/client-vehicle-3')
+  }
+
+  })
+
+// Run this code when a form is submitted to 'partnerRegularUseVehicle'
+router.post('/partnerRegularUseVehicle', function (req, res) {
+
+  // Make a variable and give it the value from 'client-vehicle-use'
+  var partnerRegularUseVehicle = req.session.data['partner-vehicle-use']
+
+  // Check whether the variable matches a condition
+  if (partnerRegularUseVehicle == "No"){
+    // Skip vehicle q's and go to assets q's
+    res.redirect('/assets-partner')
+  } else {
+    // Send user to next vehicle q to answer all financial q's
+  }
+
+  })
+
+// Run this code when a form is submitted to 'partnerBuyVehicleThree'
+router.post('/partnerBuyVehicleThree', function (req, res) {
+
+  // Make a variable and give it the value from 'client-vehicle-three'
+  var partnerBuyVehicleThree = req.session.data['partner-vehicle-three']
+
+  // Check whether the variable matches a condition
+  if (partnerBuyVehicleThree == "Yes"){
+    // Skip vehicle q's and go to assets q's
+    res.redirect('/assets-partner')
+  } else {
+    // Send user to next vehicle q to answer all financial q's
+    res.redirect('/partner-vehicle-4')
+  }
+
+  })
 
 
 module.exports = router
