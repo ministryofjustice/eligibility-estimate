10228e9b4d4f164e713f55095228c573dee40d7c
lelpalfrey <leslie.palfrey@digital.justice.gov.uk>
2022-09-29 13:21:54 +0100

    new content added after content review

--- app/config.js
+++ app/config.js
@@ -4,7 +4,8 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Estimate financial eligibility for legal aid',
+  //serviceName: 'Estimate financial eligibility for legal aid',
+  serviceName: 'Does your client qualify for legal aid?',
 
   // Default port that prototype runs on
   port: '3000',
